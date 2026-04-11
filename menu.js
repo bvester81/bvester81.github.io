9526885196a12bd844dbb90f94c2db94d8563b91
bvester81 <bvester81@gmail.com>
2026-04-11 23:30:00 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -32,8 +32,9 @@ const headerHTML = `
                     Ekspertiser <span class="arrow">▼</span>
                 </div>
                 <ul class="sub-menu" id="expertiseSub">
-                    <li><a href="lokal-ai.html" class="nav-link">AI review af håndskrift</a></li>
                     <li><a href="aveva_operations.html" class="nav-link">AVEVA Operations</a></li>
+                    <li><a href="oee_solutions.html" class="nav-link">OEE & Maskinintegration</a></li>
+                    <li><a href="lokal-ai.html" class="nav-link">AI review af håndskrift</a></li>
                 </ul>
             </li>
             <li>
